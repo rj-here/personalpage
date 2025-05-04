09d95a6295d2eccc11fd706b205a2f125d935ea5
rj-here <r1sh1j@my.yorku.ca>
2025-05-04 11:32:21 -0400

    Setting things up for NRR

---
+++ calculateNRR.js
@@ -0,0 +1,14 @@
+function getNRR() {
+var output = document.getElementById("output");
+var runsScoredFor = parseInt(prompt("Input the number of runs scored by Team 1 (FOR)"));
+var oversFor = parseInt(prompt("Input the number of overs faced in full by Team 1 (FOR). If partial overs have been done (such as 47.2), then just put 47."));
+var extras = parseInt(extras);
+var runsScoredAgainst = parseInt(prom);
+var oversAgainst = document.getElementById("oversAgainst").value;
+oversAgainst = parseInt(oversAgainst);
+var oversForTotal = (oversFor*6 + extras)/6;
+var forRR = runsScoredFor/oversForTotal;
+var againstRR = runsScoredAgainst/oversAgainst;
+var nrr = forRR - againstRR;
+document.getElementById("nrr").innerHTML = "Net Run Rate: " + nrr.toFixed(3);
+}
