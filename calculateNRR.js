238b503b71adba50e8f9c30c272b2ce621a0dd18
rj-here <r1sh1j@my.yorku.ca>
2025-05-08 21:41:15 -0400

    Lots of changes!
    About time I pick up pace on this!

--- calculateNRR.js
+++ calculateNRR.js
@@ -3,12 +3,12 @@ var output = document.getElementById("output");
 var runsScoredFor = parseInt(prompt("Input the number of runs scored by Team 1 (FOR)"));
 var oversFor = parseInt(prompt("Input the number of overs faced in full by Team 1 (FOR). If partial overs have been done (such as 47.2), then just consider 47."));
 var extras = parseInt(prompt("Extras please!"));
-var runsScoredAgainst = parseInt(prom);
-var oversAgainst = document.getElementById("oversAgainst").value;
-oversAgainst = parseInt(oversAgainst);
+var runsScoredAgainst = parseInt(prompt("Input the number of runs scored by Team 2 (AGAINST)"));
+var oversAgainst = parseInt(prompt("Input the number of maximum overs that could be faced by Team 2 (AGAINST). So, if a game had them all out for 47 overs, 50 will still be the value taken."));
 var oversForTotal = (oversFor*6 + extras)/6;
 var forRR = runsScoredFor/oversForTotal;
 var againstRR = runsScoredAgainst/oversAgainst;
 var nrr = forRR - againstRR;
 document.getElementById("nrr").innerHTML = "Net Run Rate: " + nrr.toFixed(3);
+output.innerHTML = "Net Run Rate: " + nrr.toFixed(3);
 }
