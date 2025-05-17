0f6c24fa572daabb36d68e351ad5626517b88451
rj-here <r1sh1j@my.yorku.ca>
2025-05-16 22:36:56 -0400

    SR updates
    Now due to calculate SR

--- BatStatCalculating.js
+++ BatStatCalculating.js
@@ -1,3 +1,8 @@
 function getStrikeRate() {
-    
+    var output = document.getElementById("output");
+    var runsScored = parseInt(prompt("Input the number of runs scored by the batter"));
+    var ballsFaced = parseInt(prompt("Input the number of balls faced by the batter"));
+    var strikeRate = (runsScored / ballsFaced) * 100;
+    document.getElementById("strikeRate").innerHTML = "Strike Rate: " + strikeRate.toFixed(2);
+    output.innerHTML = "Strike Rate: " + strikeRate.toFixed(2);
 }
