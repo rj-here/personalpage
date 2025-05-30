f259520d1da1e643f14bf8cb117fcff002441ff5
rj-here <r1sh1j@my.yorku.ca>
2025-05-29 20:31:09 -0400

    Bowling Calculations
    Now updating bowling statistics. Calculate away

--- BowlStatCalculating.js
+++ BowlStatCalculating.js
@@ -1,9 +1,24 @@
 function calculateBowlingAverage() {
-
+var output = document.getElementById("output");
+var runsConceded = parseInt(prompt("Input the number of runs conceded by the bowler"));
+var wicketsTaken = parseInt(prompt("Input the number of wickets taken by the bowler"));
+var avg = (runsConceded / wicketsTaken);
+document.getElementById("bowlingAverage").innerHTML = "Bowling Average: " + avg.toFixed(2);
+output.innerHTML = "Bowling Average: " + avg;
 }
 function calculateBowlingStrikeRate() {
-
+var output = document.getElementById("output");
+var ballsBowled = parseInt(prompt("Input the number of balls bowled by the bowler"));
+var wicketsTaken = parseInt(prompt("Input the number of wickets taken by the bowler"));
+var strikeRate = (ballsBowled / wicketsTaken);
+document.getElementById("bowlingStrikeRate").innerHTML = "Bowling Strike Rate: " + strikeRate.toFixed(2);
+output.innerHTML = "Bowling Strike Rate: " + strikeRate.toFixed(2);
 }
 function calculateEconomyRate() {
-
+var output = document.getElementById("output");
+var runsConceded = parseInt(prompt("Input the number of runs conceded by the bowler"));
+var bowlsBowled = parseInt(prompt("Input the number of balls bowled by the bowler"));
+var economyRate = (runsConceded / (bowlsBowled / 6));
+document.getElementById("economyRate").innerHTML = "Economy Rate: " + economyRate.toFixed(2);
+output.innerHTML = "Economy Rate: " + economyRate.toFixed(2);
 }
