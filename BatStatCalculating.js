528a7ff77d0604214ac09f90292d77427b12d7ac
rj-here <r1sh1j@my.yorku.ca>
2025-06-19 09:32:37 -0400

    Adding comments
    Adding comments to the files, so it does not look like random stuff to those accessing the repository

--- BatStatCalculating.js
+++ BatStatCalculating.js
@@ -1,17 +1,19 @@
 function getStrikeRate() {
-    var output = document.getElementById("output");
-    var runsScored = parseInt(prompt("Input the number of runs scored by the batter"));
-    var ballsFaced = parseInt(prompt("Input the number of balls faced by the batter"));
-    var strikeRate = (runsScored / ballsFaced) * 100;
-    document.getElementById("strikeRate").innerHTML = "Strike Rate: " + strikeRate.toFixed(2);
-    output.innerHTML = "Strike Rate: " + strikeRate.toFixed(2);
+    // This function gets the strike rate of a batter: runs scored / balls faced * 100
+    var output = document.getElementById("output"); // The output element to display the strike rate
+    var runsScored = parseInt(prompt("Input the number of runs scored by the batter")); // This prompt gets the runs scored by the batter
+    var ballsFaced = parseInt(prompt("Input the number of balls faced by the batter")); // This prompt gets the number of balls faced by the batter
+    var strikeRate = (runsScored / ballsFaced) * 100; // The calculation of strike rate: runs scored over the balls faced, multiplied by 100
+    document.getElementById("strikeRate").innerHTML = "Strike Rate: " + strikeRate.toFixed(2); // Updating the HTML element with the strike rate
+    output.innerHTML = "Strike Rate: " + strikeRate.toFixed(2); // Displaying the strike rate
 }
 
 function getAverage() {
-    var output = document.getElementById("output");
-    var runsScored = parseInt(prompt("Input the number of runs scored by the batter"));
-    var dismissals = parseInt(prompt("Input the number of times the batter has been dismissed"));
-    var average = (runsScored / dismissals).toFixed(2);
-    document.getElementById("average").innerHTML = "Batting Average: " + average;
-    output.innerHTML = "Batting Average: " + average;
+    // This function gets the batting average of a batter: runs scored / dismissals
+    var output = document.getElementById("output"); // The output element to display the batting average
+    var runsScored = parseInt(prompt("Input the number of runs scored by the batter")); // This prompt gets the runs scored by the batter
+    var dismissals = parseInt(prompt("Input the number of times the batter has been dismissed")); // This prompt gets the number of dismissals of the batter
+    var average = (runsScored / dismissals).toFixed(2); //The batting average is the runs scored divided by the dismissals
+    document.getElementById("average").innerHTML = "Batting Average: " + average; // Updating the HTML element with the batting average
+    output.innerHTML = "Batting Average: " + average; // Displaying the batting average
 }
--- calculateNRR.js
+++ calculateNRR.js
@@ -1,14 +1,15 @@
 function getNRR() {
-var output = document.getElementById("output");
-var runsScoredFor = parseInt(prompt("Input the number of runs scored by Team 1 (FOR)"));
-var oversFor = parseInt(prompt("Input the number of overs faced in full by Team 1 (FOR). If partial overs have been done (such as 47.2), then just consider 47."));
-var extras = parseInt(prompt("Extras please!"));
-var runsScoredAgainst = parseInt(prompt("Input the number of runs scored by Team 2 (AGAINST)"));
-var oversAgainst = parseInt(prompt("Input the number of maximum overs that could be faced by Team 2 (AGAINST). So, if a game had them all out for 47 overs, 50 will still be the value taken."));
-var oversForTotal = (oversFor*6 + extras)/6;
-var forRR = runsScoredFor/oversForTotal;
-var againstRR = runsScoredAgainst/oversAgainst;
-var nrr = forRR - againstRR;
-document.getElementById("nrr").innerHTML = "Net Run Rate: " + nrr.toFixed(3);
-output.innerHTML = "Net Run Rate: " + nrr.toFixed(3);
+    // This function calculates the Net Run Rate (NRR) for a cricket match: the difference between the run rate of the 2 teams
+var output = document.getElementById("output"); // The output element to display the Net Run Rate
+var runsScoredFor = parseInt(prompt("Input the number of runs scored by Team 1 (FOR)")); // This prompt gets the runs scored by Team 1
+var oversFor = parseInt(prompt("Input the number of overs faced in full by Team 1 (FOR). If partial overs have been done (such as 47.2), then just consider 47.")); // This prompt gets the number of voers faced by first team
+var extras = parseInt(prompt("How many balls of the not fully completed over were faced?")); // This prompt gets the additonal balls faced of the not fully completed over (2 balls in the case of the 47.2 overs example)
+var runsScoredAgainst = parseInt(prompt("Input the number of runs scored by Team 2 (AGAINST)")); // This prompt gets the runs scored by Team 2 
+var oversAgainst = parseInt(prompt("Input the number of maximum overs that could be faced by Team 2 (AGAINST). So, if a game had them all out for 47 overs, 50 will still be the value taken.")); // This prompt gets the maximum overs that could be faced by Team 2, so whether they batted a full 50 overs, or lost all 10 wickets in 47 overs, it'll be 50 for that given game
+var oversForTotal = (oversFor*6 + extras)/6; // This calculation is converts the overs faced by Team 1 into an eligible value, for calcuation purposes
+var forRR = runsScoredFor/oversForTotal; // This calcuation is the run rate of Team 1, or the FOR team
+var againstRR = runsScoredAgainst/oversAgainst; // This calculation is the run rate of Team 2, or the AGAINST team
+var nrr = forRR - againstRR; // This calcuation is the NRR: for - against
+document.getElementById("nrr").innerHTML = "Net Run Rate: " + nrr.toFixed(3); // Updating the HTML element with the NRR
+output.innerHTML = "Net Run Rate: " + nrr.toFixed(3); // Displaying the NRR
 }
