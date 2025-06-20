3b9007dcb4e9542aad538d573e0c52c6f79b2bbd
rj-here <r1sh1j@my.yorku.ca>
2025-06-20 11:49:44 -0400

    Many modifications
    Moving around many things, as part of a restructuring process.

--- offToProjects.js
+++
@@ -1,9 +0,0 @@
-function project1() {
-    window.open("NRRCalculations.html");
-}
-function project2() {
-    window.open("BatStats.html");
-}
-function project3() {
-    window.open("BowlStats.html");
-}
---
+++ openTheseSites.js
@@ -0,0 +1,9 @@
+function lbwNRR() {
+    window.open("https://rj-here.github.io/lbwincricket/netRunRate.html");
+}
+function lbwBatStats() {
+    window.open("https://rj-here.github.io/lbwincricket/battingStats.html");
+}
+function lbwBowlStats() {
+    window.open("https://rj-here.github.io/lbwincricket/bowlingStats.html");
+}
