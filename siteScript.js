aa15c506943635c20c7059f9d968d7d2d12f694b
rj-here <r.rishi.rj@gmail.com>
2026-01-01 08:11:13 -0500

    Update siteScript.js

--- siteScript.js
+++ siteScript.js
@@ -62,7 +62,6 @@ function modeToggle() {
         localStorage.setItem('theme', 'light'); //Maintains light mode
     }
 }
-miniAnnounce(); //To call the mini announcement function on page load
 typeWriter(); //To start the typing effect
 //This will be for sorting & filtering projects
 function filterAndSort() {
